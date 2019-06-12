732bb6def1cfa29df5d3b093e2b539e7b544f6da
osgonzalez <osgonzalez@esei.uvigo.es>
2019-06-12 07:10:05 -0400

    First Commit

---
+++ LibsPipDownloader.py
@@ -0,0 +1,31 @@
+import os
+
+# scipy
+os.system("pip install scipy")
+import scipy
+print('scipy: %s' % scipy.__version__)
+
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+
+# matplotlib
+os.system("pip install matplotlib")
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+
+# pandas
+os.system("pip install pandas")
+import pandas
+print('pandas: %s' % pandas.__version__)
+
+# statsmodels
+os.system("pip install statsmodels")
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+
+# scikit-learn
+os.system("pip install sklearn")
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
+
